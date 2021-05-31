f158f258ff9a6a5e9704fb4516e3677c614bad9b
Arslan Gait <71497132+arslan-gait@users.noreply.github.com>
2021-05-31 19:04:21 +0600

    Create 31.05_maxmum_depth_of_binary_tree.py

---
+++ 31.05_maxmum_depth_of_binary_tree.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+class Solution:
+  
+  # iterative
+  def maxDepth(self, root: TreeNode) -> int:
+    if root == None:
+      return 0
+    nodes = [root]
+    level = 0
+    while nodes:
+      for _ in range(len(nodes)):
+        cur = nodes.pop(0)
+        if cur.left != None:
+          nodes.append(cur.left)
+        if cur.right != None:
+          nodes.append(cur.right)
+      level += 1
+    return level
+
+  # recursive
+  def maxDepth2(self, root: TreeNode) -> int:
+    if root == None:
+      return 0
+    return max(self.maxDepth(root.left) + 1, self.maxDepth(root.right) + 1)

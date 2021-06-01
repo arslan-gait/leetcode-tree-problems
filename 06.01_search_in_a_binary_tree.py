445af3961e52d50bd0c9c00050b442e9f66bc641
Arslan Gait <71497132+arslan-gait@users.noreply.github.com>
2021-06-02 00:23:53 +0600

    Update 06.01_search_in_a_binary_tree.py

--- 06.01_search_in_a_binary_tree.py
+++ 06.01_search_in_a_binary_tree.py
@@ -1,6 +1,7 @@
 # https://leetcode.com/problems/search-in-a-binary-search-tree/
-# recursive
 class Solution:
+    
+    # recursive
     def searchBST(self, root: TreeNode, val: int) -> TreeNode:
         if root is None:
             return None
@@ -10,3 +11,15 @@ class Solution:
             return self.searchBST(root.right, val)
         else:
             return self.searchBST(root.left, val)
+    
+    # iterative
+    def searchBST2(self, root: TreeNode, val: int) -> TreeNode:
+        cur = root
+        while cur:
+            if cur.val == val:
+                return cur
+            elif cur.val > val:
+                cur = cur.left
+            else:
+                cur = cur.right
+        return cur

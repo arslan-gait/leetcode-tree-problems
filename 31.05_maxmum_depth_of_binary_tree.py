ea550603b0c8d285df51c08fa7af4dea2075240b
Arslan Gait <71497132+arslan-gait@users.noreply.github.com>
2021-06-02 00:08:21 +0600

    Update 31.05_maxmum_depth_of_binary_tree.py

--- 31.05_maxmum_depth_of_binary_tree.py
+++ 31.05_maxmum_depth_of_binary_tree.py
@@ -22,4 +22,4 @@ class Solution:
   def maxDepth2(self, root: TreeNode) -> int:
     if root == None:
       return 0
-    return max(self.maxDepth(root.left) + 1, self.maxDepth(root.right) + 1)
+    return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1

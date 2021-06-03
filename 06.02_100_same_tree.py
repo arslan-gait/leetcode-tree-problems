38bf0ecb50c3db0e74a8dedcf5636a60791c080c
Arslan Gait <71497132+arslan-gait@users.noreply.github.com>
2021-06-03 15:08:30 +0600

    Update 06.02_100_same_tree.py

--- 06.02_100_same_tree.py
+++ 06.02_100_same_tree.py
@@ -3,32 +3,30 @@ from collections import deque
 
 class Solution:
     # iterative
-    # space and time complexity - O(nodes(p) + nodes(q))
+    # space and time complexity - O(min(nodes(p), nodes(q)))
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
-        if not p and not q:
-            return True
-        queue = deque([(p,q)])
-        
-        while queue:
-            curP, curQ = queue.popleft()
-            if not curP and not curQ:
+        stack = [(p, q)]
+        while len(stack) > 0:
+            first, second = stack.pop()
+            if first is None and second is None:
                 continue
-            if not curP or not curQ:
+            if first is None or second is None:
                 return False
-            if curP.val != curQ.val:
+            assert(first is not None and second is not None)
+            if first.val != second.val:
                 return False
-            
-            queue.append((curP.left, curQ.left))
-            queue.append((curP.right, curQ.right))
-
+            stack.append((first.left, second.left))
+            stack.append((first.right, second.right))
         return True
     
     # recursive
-    # time complexity  - O(nodes(p) + nodes(q))
-    # space complexity - O(height(p) + height(q))
+    # space and time complexity - O(min(nodes(p), nodes(q)))
     def isSameTree2(self, p: TreeNode, q: TreeNode) -> bool:
-        if not p and not q:
+        if p is None and q is None:
             return True
-        if not p or not q or p.val != q.val:
+        if p is None or q is None:
             return False
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return \
+            p.val != q.val and \
+            self.isSameTree(p.left, q.left) and \
+            self.isSameTree(p.right, q.right)

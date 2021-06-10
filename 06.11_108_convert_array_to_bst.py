9e2bff1be56130813f6fdfd1a802595300feab8c
Arslan Gait <71497132+arslan-gait@users.noreply.github.com>
2021-06-11 00:07:01 +0600

    Create 06.11_108_convert_array_to_bst.py

---
+++ 06.11_108_convert_array_to_bst.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/submissions/
+class Solution:
+    
+    # recursive
+    # time complexity  - O(log(nodes(root)))
+    # space complexity - O(nodes(root)) to store new tree
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+
+        def helper(left: int, right: int) -> TreeNode:
+            if right < left:
+                return None
+            mid = (right - left) // 2 + left
+            t = TreeNode(nums[mid])
+            t.left = helper(left, mid - 1)   # T(n/2) -> O(logn)
+            t.right = helper(mid + 1, right) # T(n/2) -> O(logn)
+            return t
+        
+        return helper(0, len(nums) - 1)

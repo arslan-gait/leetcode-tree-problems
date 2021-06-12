f48c80977cc1d7124adb7b2a95fdcfd90b11b408
Arslan Gait <71497132+arslan-gait@users.noreply.github.com>
2021-06-12 15:45:34 +0600

    Update 06.11_671_second_minimum_node_in_bt.py

--- 06.11_671_second_minimum_node_in_bt.py
+++ 06.11_671_second_minimum_node_in_bt.py
@@ -5,8 +5,9 @@ class Solution:
         self.ans = -1
 
     # recursive
-    # time & space complexity - O(nodes(root))
-    def findSecondMinimumValue(self, root: TreeNode) -> int:
+    # time complexity  - O(nodes(root))
+    # space complexity - O(height(root))
+    def findSecondMinimumValue2(self, root: TreeNode) -> int:
         
         def visit(node: TreeNode):
             if node is None:
@@ -21,3 +22,44 @@ class Solution:
         
         visit(root)
         return self.ans
+    
+    # iterative BFS
+    # time & space complexity - O(nodes(root))
+    def findSecondMinimumValue3(self, root: TreeNode) -> int:
+        ans = -1
+        curLevel = [root]
+        while curLevel:
+            nextLevel = []
+            for node in curLevel:
+                if node.val > root.val:
+                    if ans == -1 or node.val < ans:
+                        ans = node.val
+                    continue
+                else:
+                    if node.left is not None:
+                        nextLevel.append(node.left)
+                    if node.right is not None:
+                        nextLevel.append(node.right)
+            curLevel = nextLevel
+        return ans
+
+    # iterative DFS
+    # time complexity  - O(nodes(root))
+    # space complexity - O(height(root))
+    def findSecondMinimumValue(self, root: TreeNode) -> int:
+        ans = -1
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            
+            if node.val > root.val:
+                if ans == -1 or node.val < ans:
+                    ans = node.val
+                continue
+            else:
+                if node.left is not None:
+                    stack.append(node.left)
+                if node.right is not None:
+                    stack.append(node.right)
+
+        return ans

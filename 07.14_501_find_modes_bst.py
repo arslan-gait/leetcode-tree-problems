e1f2c02dee2bcdd9f33c482bd8d226f33b5d8df9
Arslan Gait <71497132+arslan-gait@users.noreply.github.com>
2021-07-14 23:55:48 +0600

    501: added solution w/o extra space

--- 07.14_501_find_modes_bst.py
+++ 07.14_501_find_modes_bst.py
@@ -42,3 +42,48 @@ class Solution:
         visit(root)
         
         return modes
+
+    
+    # recursive (same as above, but no extra space)
+    # time complexity  - O(nodes(root))
+    # space complexity - O(height(root))
+    def findMode(self, root: TreeNode) -> List[int]:
+        
+        cnt, maxCnt, prev = 1, 0, 100_001
+        modes = []
+
+        
+        def handleVal(val: int, isSecond: bool):
+            
+            nonlocal cnt, maxCnt, prev
+            
+            if val == prev:
+                cnt += 1
+            else:
+                cnt = 1
+            
+            if cnt > maxCnt:
+                maxCnt = cnt
+                
+            if isSecond and cnt == maxCnt:
+                modes.append(val)
+            
+            prev = val
+        
+        
+        # in-order traversal
+        def visit(node: TreeNode, isSecond: bool):
+            
+            if node is None:
+                return
+            
+            visit(node.left, isSecond)
+            handleVal(node.val, isSecond)                        
+            visit(node.right, isSecond)           
+        
+        
+        visit(root, False)
+        cnt, prev = 1, 100_001
+        visit(root, True)
+        
+        return modes
